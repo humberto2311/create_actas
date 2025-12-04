760462887ae300de2c087d7f96f0ff6db3d32afb
Humberto Lozano <aacendra@ugpp.gov.co>
2025-12-04 16:51:08 -0500

    Initialize working environment

---
+++ src/main/java/com/app/juridico/create_actas/CreateActasApplication.java
@@ -0,0 +1,13 @@
+package com.app.juridico.create_actas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateActasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateActasApplication.class, args);
+	}
+
+}
